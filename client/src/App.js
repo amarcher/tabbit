34efc0b3ac5ec5381c44d4d69e50be45714abbb5
Russell Salerno <russell.j.salerno@gmail.com>
2017-04-22 17:34:56 -0700

    moved login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Auth from './auth';
-import Ajax from './ajax';
+import './app.css';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			email: '',
-			password: '',
-		};
-
-		this.bindEventHandlers();
-	}
-
-	onClick() {
-		Auth.login(this.state).then(() => {
-			Ajax.get('/api/v1/tabs').then(console.log); // eslint-disable-line no-console
-		});
-	}
-
-	onEmailChange(event) {
-		this.setState({ email: event.target.value });
-	}
-
-	onPasswordChange(event) {
-		this.setState({ password: event.target.value });
-	}
-
-	bindEventHandlers() {
-		this.onClick = this.onClick.bind(this);
-		this.onEmailChange = this.onEmailChange.bind(this);
-		this.onPasswordChange = this.onPasswordChange.bind(this);
-	}
-
+export default class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to Tabbit Rabbit</h2>
-				</div>
-
-				<input
-					placeholder="User email"
-					type="text"
-					defaultValue={this.state.value}
-					onChange={this.onEmailChange}
-				/>
-
-				<input
-					placeholder="Password"
-					type="password"
-					defaultValue={this.state.value}
-					onChange={this.onPasswordChange}
-				/>
-
-				<button onClick={this.onClick}>Login </button>
+			<div>
+				{React.cloneElement(this.props.children, this.props)}
 			</div>
 		);
 	}
 }
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './App';
 import './index.css';
 
+// Components
+import Login from './components/Login';
+
+console.log(Login);
+
+const router = (
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			<IndexRoute component={Login} />
+			<Route component={Login} />
+		</Route>
+	</Router>
+);
+
 ReactDOM.render(
-	<App />,
-	document.getElementById('root'),
+	router, document.getElementById('root'),
 );
