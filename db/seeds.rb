3a72d574f6f265df4dcf95a7e19919e825d9c9dd
Andrew Archer <andrew.m.archer@gmail.com>
2017-04-23 10:44:41 -0700

    use better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
 user = User.new({
-  name: 'Andrew',
-  email: 'aarcher520@gmail.com',
-  phone_number: '415-555-5555',
+	name: 'Andrew',
+	email: 'aarcher520@gmail.com',
+	phone_number: '415-555-5555',
 })
 
 user.password = 'testdb'
 user.save!
+
+rabbit = Rabbit.new({
+	name: 'Andrew',
+	email: 'aarcher520@gmail.com',
+	phone_number: '415-555-5555',
+})
+
+rabbit.user = user
+rabbit.save!
+
+tab = Tab.new(name: "Untitled Tab")
+tab.items << Item.create(price: 0)
+tab.user = user
+tab.rabbits << rabbit
+tab.save!
